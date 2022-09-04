30b5520ebed2e197269da9d177ae117f41bda912
beckireby15@gmail.com <beckireby15@gmail.com>
2022-09-04 23:12:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name, email) {
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(name, email),
+  };
+}
+
+return fetch("http://localhost:3000/users", configObj)
+  .then((resp) => resp.json())
+  .then((obj) => {
+    document.body.innerHTML = obj.id;
+  })
+  .catch((err) => {
+    alert("Not found");
+
+    document.body.innerHTML = err.message;
+  });
